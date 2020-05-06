8ca8af8210522820f3f0e6216a7bd36aea0e91aa
Aleksandr <SAUSS@protonmail.com>
2020-05-06 13:43:13 +0300

    2

--- lab7.cpp
+++ lab7.cpp
@@ -9,19 +9,24 @@
 // y" + y = x*exp(-x)  y(0) = 1, y'(0) = 0; x->[0,2]
 
 
-
-double yp_x(const double &x,const double &y){
-    return -pow(x,2)/2;
+double a_f(const double &t){
+    return cos(t)/2 + (exp(-t) * cos(t) * (cos(t) + t * cos(t) + t * sin(t)))/2 + (exp(-t)*sin(t)*(sin(t)-t*cos(t)+t*sin(t)))/2;
 }
 
-double ypp_x (const double &x,const double &y){
-    return -(1+x)*exp(-x) - y*x ;
+double aa_f(const double &t){
+
+    return -(exp(t) * sin(t) + t) / (2 * exp(t));
 }
 
-double ypp_y(const double &x,const double &y1){
-    return -1;
+
+double y_1(){
+    return 0;   // => y'(0)/1! = 0
 }
- 
+
+double y_2(const double &x,const double &y0){
+    return x*exp(-x) - y0;   // => y"(0)*x^2/2! =  - x^2/2!
+}
+
 double norma(double x0, double y0, double x1, double y1)
 {
 	return sqrt(pow(x0 - x1, 2) + pow(y0 - y1, 2));
@@ -31,24 +36,34 @@ double norma(double x0, double y0, double x1, double y1)
 
 int main(){
     int N = 10000;
-    double res[N] = {0};
-    const double h = 0.01;
+    double y[N] = {0};
+    double yy[N] = {0};
+    double a[N] = {0};
+    const double h = 0.01; //step
     double x = 0.0;
-    res[0] = 0;
     int i = 0;
+    y[i] = 1;
+    yy[i] = 0;
+
     while(x <= 2){
-        res[i + 1] =  res[i] + h*(ypp_x(x,res[i]) + (h/2) *(-1)*(yp_x(x,res[i]) + yp_x(x,res[i])));
-        x += 0.01;
+        y[i+1] = 1 -  y_1()*x + (pow(x,2)*(y_2(x,y[i])))/2;
+        yy[i+1] = -x +  y_1()*x + (pow(x,2)*(y_2(x,yy[i])))/2;
+        a[i] = a_f(x);
+        x += h;
         i++;
     }
 
-    std::ofstream out;
-    out.open("data.dat");
+    std::ofstream out1,out2;
+    out1.open("y.dat");
+    out2.open("yy.dat");
+
 
     for(int j = 0; j < i; j++){
-        out << res[j] << std::endl;
+        out1 << y[j] << std::endl;
+        out2 << a[j] << std::endl;
     }
-    out.close();
+    out1.close();
+    out2.close();
     
 
 
